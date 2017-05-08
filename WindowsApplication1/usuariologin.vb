1c3a3a7b54a33aaf441bf290e7b2c38f695244d1
Luciana <luciana.escalada94@gmail.com>
2017-05-07 21:52:26 -0300

    ultimoArreglo

--- WindowsApplication1/usuariologin.vb
+++ WindowsApplication1/usuariologin.vb
@@ -31,4 +31,8 @@ Public Class usuariologin
     Private Sub BtnCancelar_Click(sender As Object, e As EventArgs) Handles BtnCancelar.Click
         Me.Close()
     End Sub
+
+    Private Sub usuariologin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
